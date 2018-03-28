5aa82bd1e0eab74ad7359509f3bf5cd93d1d5ec8
Adrian Herbert <aherbert2020@bklawtech.com>
2018-03-28 22:29:38 +0000

    I copied and oasted to c9.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,34 @@
 
 $(document).ready(function(){
   
-  
+  function giphyURLWithSearchTerm(searchTerm) {
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+}
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('body').append('<img src=' + srcURL + '>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+           var url = response.data[0].images.original.url;
+           appendImageToBody(url);
+      },
+    }); 
+}
+
+//Add a click handler beloe to call the function when the button is clicked
+  $("button").click(function(){
+        var searchTerm = $("input").val();
+        
+    callGiphyAPIWithSearchTerm(searchTerm);
+    });
   
   
 });
